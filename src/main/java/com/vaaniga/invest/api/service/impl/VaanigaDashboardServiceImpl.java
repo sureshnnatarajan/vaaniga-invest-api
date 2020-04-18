1fdf83f48e579ea5f6a80094daac576e244c6ff2
Suresh Natarajan <suresh.n.natarajan@gmail.com>
2020-04-18 16:24:00 +0530

    Added dashboard controller

--- src/main/java/com/vaaniga/invest/api/service/impl/VaanigaDashboardServiceImpl.java
+++ src/main/java/com/vaaniga/invest/api/service/impl/VaanigaDashboardServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vaaniga.invest.api.model.CompanyMaster;
+import com.vaaniga.invest.api.model.CompanyProducts;
 import com.vaaniga.invest.api.repository.CompanyMasterRepository;
 import com.vaaniga.invest.api.repository.CompanyProductsRepository;
 import com.vaaniga.invest.api.service.VaanigaDashboardService;
@@ -29,9 +30,27 @@ public class VaanigaDashboardServiceImpl implements VaanigaDashboardService {
 
 	@Override
 	public List<JSONObject> getProductCountByCompanies(String sectorName) {
+		
 		List<JSONObject> resultList = new ArrayList<>();
 		
+		//Note: Since it is like search results are fetched even for a single character. So, pass complete sector name.
 		List<CompanyMaster> companyList = companyMasterRepo.findByCategoriesContainingIgnoreCase(sectorName);
+		
+		for (CompanyMaster company : companyList) {
+			List<CompanyProducts> productList = companyProductsRepo.findByCompanyId(company.getId());
+			
+			JSONObject productCount = new JSONObject(); 
+			
+			productCount.put("companyName", company.getCompanyName());
+			
+			if (null == productList || productList.isEmpty()) {
+				productCount.put("count", 0);
+			} else {
+				productCount.put("count", productList.size());
+			}
+			
+			resultList.add(productCount);
+		}
 		
 		return resultList;
 	}
